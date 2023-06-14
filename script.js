d70ffab0a98b043bfea589af8209e47fef8cb911
Julio Cesar Estrada Ramos <julio.estradara@gmail.com>
2023-06-14 11:57:31 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+
+        slidesPerView: 1,
+        spacebetween: 30,
+        grabCursor: true,
+        loop: true,
+        breakpoints: {
+            991: {
+                slidesPerView: 1,
+            }
+        }
+});
